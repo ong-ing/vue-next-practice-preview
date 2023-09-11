8f36e07eb4435eb04c8d6f9729b4bc6587e96f0c
huangle <huangle@naturehike.com>
2023-09-11 09:51:38 +0800

    add sync menu

---
+++ assets/js/index-1a396cb9.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o,c as t,a as c}from"./@vue-cba46ac6.js";const n={},s=c("h1",null,"测试页面",-1),r=[s];function _(a,d){return o(),t("div",null,r)}const f=e(n,[["render",_]]);export{f as default};
